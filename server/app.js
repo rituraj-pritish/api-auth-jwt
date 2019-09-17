592b80b94923692aa0ddfa67fae67bbfcb0f5ee3
rituraj-pritish <riturajpritish@gmail.com>
2019-09-17 11:42:30 +0530

    optimized

--- server/app.js
+++ server/app.js
@@ -22,4 +22,12 @@ require('./services/passport');
 
 app.use('/users', require('./routes/users'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+  );
+}
+
 module.exports = app;
